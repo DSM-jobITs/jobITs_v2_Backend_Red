5c01887972fef1e26ede504ba8be04516ab714b4
MinJunSeo <smj0385@naver.com>
2020-12-12 17:02:43 +0900

    feat: app.ts init

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+import morgan from "morgan";
+import cors from "cors";
+import path from "path";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: path.join(__dirname + "../.env") });
+
+const app: Application = express();
+
+app.use(morgan("dev"));
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use((err, req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({ message: err.message });
+});
+
+app.listen(3000, () => {
+  console.log("Server listen at 3000 port!");
+});
