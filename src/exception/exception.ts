e254a99ef5eb2a8b1b4f0072274c33d33e2300a1
MinJunSeo <smj0385@naver.com>
2021-01-03 11:14:04 +0900

    feat: class member variable status

--- src/exception/exception.ts
+++ src/exception/exception.ts
@@ -1,7 +1,8 @@
 export class HttpError extends Error {
-  constructor(status: number, message?: string) {
+  public status: number;
+
+  constructor(status: number, message: string) {
     super(message);
-    this.name = "HttpError";
     this["status"] = status;
   }
 }
