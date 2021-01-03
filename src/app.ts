1ffdcd797c3cfdc6ce33430fd802df2c71be1d9a
MinJunSeo <smj0385@naver.com>
2021-01-03 11:36:23 +0900

    fix: use loader and logger

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,12 @@
-import express, { Application, Request, Response, NextFunction } from "express";
-import morgan from "morgan";
-import cors from "cors";
-import path from "path";
-import * as dotenv from "dotenv";
+import express from "express";
+import loader from "./loaders";
+import config from "./config";
+import logger from "./loaders/logger";
 
-dotenv.config({ path: path.join(__dirname + "../.env") });
+const app = express();
 
-const app: Application = express();
+loader(app);
 
-app.use(morgan("dev"));
-app.use(cors());
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use((err, req: Request, res: Response, next: NextFunction) => {
-  res.status(404).json({ message: err.message });
-});
-
-app.listen(3001, () => {
-  console.log("Server listen at 3000 port!");
+app.listen(config.port, () => {
+  logger.info(`Server listening on ${config.port}`);
 });
