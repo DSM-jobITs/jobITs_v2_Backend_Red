741e1667cd015e3bccf7d75c3601ae2f3caf724b
MinJunSeo <smj0385@naver.com>
2021-01-03 11:23:03 +0900

    fix: string error

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,10 +5,10 @@ config();
 export default {
   port: Number(process.env.SERVER_PORT),
   mysql: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    name: process.env.DB_NAME
+    host: process.env.DB_HOST || "",
+    user: process.env.DB_USER || "",
+    password: process.env.DB_PASSWORD || "",
+    name: process.env.DB_NAME || ""
   },
-  secertKey: process.env.JWT_SECERT
+  secertKey: process.env.JWT_SECERT || ""
 };
