1d06c5bf3c0240bb1980959ff108e013c48d4423
MinJunSeo <smj0385@naver.com>
2021-03-03 19:04:37 +0900

    feat: enterprise table attributes interface

---
+++ src/interfaces/enterprise.ts
@@ -0,0 +1,11 @@
+export interface EnterPriseAttributes {
+  entNo: string;
+  name: string;
+  phone: string;
+  establishmentDate: string;
+  sales: number;
+  introduce: string;
+  sector: string;
+  address: string;
+  zipCode: string;
+}
