fdccfd5117436041e25a0d1dd66bf1ea7f413ea5
MinJunSeo <smj0385@naver.com>
2021-03-03 18:52:39 +0900

    feat: QualificationRepository

---
+++ src/repositories/definitions/qualification.ts
@@ -0,0 +1,26 @@
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "../../loaders/database";
+import { QualificationAttributes } from "../../interfaces";
+
+export class QualificationRepository extends Model<QualificationAttributes, "id"> {
+
+}
+
+QualificationRepository.init({
+  id: {
+    type: DataTypes.STRING(30),
+    primaryKey: true
+  },
+  grade: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  recuritId: {
+    type: DataTypes.STRING(30),
+    allowNull: false,
+    field: 'recurit_id'
+  }
+}, {
+  sequelize,
+  tableName: 'qualification'
+})
