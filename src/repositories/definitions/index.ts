ecb7ecfacdb2fb173ee73941c418a44a448d774f
MinJunSeo <smj0385@naver.com>
2021-03-03 20:06:03 +0900

    fix: missing specialty file

--- src/repositories/definitions/index.ts
+++ src/repositories/definitions/index.ts
@@ -6,6 +6,7 @@ export * from "./manager";
 export * from "./meal";
 export * from "./qualification";
 export * from "./recurit";
+export * from "./specialty";
 export * from "./student";
 export * from "./user";
 export * from "./welfare";
