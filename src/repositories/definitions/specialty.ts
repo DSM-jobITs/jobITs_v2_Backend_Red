fc3a18bc2129290e899bf1933ab672b4da435d3b
MinJunSeo <smj0385@naver.com>
2021-03-03 17:01:36 +0900

    feat: SpecialtyRepositroy

---
+++ src/repositories/definitions/specialty.ts
@@ -0,0 +1,26 @@
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "../../loaders/database";
+import { SpecialtyAttributes } from "../../interfaces";
+
+export class SpecialtyRepositroy extends Model<SpecialtyAttributes, "id"> {
+
+}
+
+SpecialtyRepositroy.init({
+  id: {
+    type: DataTypes.STRING(30),
+    primaryKey: true
+  },
+  specialty: {
+    type: DataTypes.STRING(20),
+    allowNull: false
+  },
+  qualificationId: {
+    type: DataTypes.STRING(30),
+    allowNull: false,
+    field: 'qualification_id'
+  }
+}, {
+  sequelize,
+  tableName: 'specialty'
+});
