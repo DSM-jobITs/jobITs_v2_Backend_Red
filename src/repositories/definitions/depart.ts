2af67516bc70b93187683e56ea8f5805e41fd962
MinJunSeo <smj0385@naver.com>
2021-03-03 19:02:41 +0900

    feat: DepartRepository

---
+++ src/repositories/definitions/depart.ts
@@ -0,0 +1,21 @@
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "../../loaders/database";
+import { DepartAttributes } from "../../interfaces";
+
+export class DepartRepository extends Model<DepartAttributes, "id"> {
+
+}
+
+DepartRepository.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true
+  },
+  dept: {
+    type: DataTypes.STRING(20),
+    allowNull: false
+  }
+}, {
+  sequelize,
+  tableName: 'dept'
+});
