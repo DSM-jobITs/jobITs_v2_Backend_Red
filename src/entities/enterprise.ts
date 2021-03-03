ec3b224663f2f485ac48c6ec731a823c30a85519
MinJunSeo <smj0385@naver.com>
2021-03-03 19:35:22 +0900

    refactor: move directory

---
+++ src/entities/enterprise.ts
@@ -0,0 +1,11 @@
+export interface EnterpriseAttributes {
+  entNo: string;
+  name: string | null;
+  phone: string | null;
+  establishmentDate: string | null;
+  sales: number | null;
+  introduce: string | null;
+  sector: string | null;
+  address: string | null;
+  zipCode: string | null;
+}
