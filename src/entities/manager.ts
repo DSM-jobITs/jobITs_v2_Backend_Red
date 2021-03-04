5e9a8048bfee31b46614f85ef28e4124448bd45a
MinJunSeo <smj0385@naver.com>
2021-03-04 15:18:20 +0900

    fix: manager entity

--- src/entities/manager.ts
+++ src/entities/manager.ts
@@ -1,6 +1,31 @@
-export interface ManagerAttributes {
-  entNo: string;
-  rank: string;
-  phone: string;
-  email: string;
+import { Model, DataType, Table, Column } from "sequelize-typescript";
+import { IsMobilePhone, IsEmail } from "class-validator";
+@Table
+class Manager extends Model {
+  @Column({
+    type: DataType.CHAR(12),
+    primaryKey: true,
+    field: 'ent_no'
+  })
+  entNo!: string;
+
+  @Column({
+    type: DataType.STRING(20),
+    allowNull: false
+  })
+  rank!: string;
+
+  @Column({
+    type: DataType.CHAR(14),
+    allowNull: false
+  })
+  @IsMobilePhone("ko-KR")
+  phone!: string;
+
+  @Column({
+    type: DataType.STRING(30),
+    allowNull: false
+  })
+  @IsEmail()
+  email!: string;
 }
