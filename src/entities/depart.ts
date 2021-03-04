7522e154177bb5d8c3ec83f958ec421ba2f80eec
MinJunSeo <smj0385@naver.com>
2021-03-04 14:52:27 +0900

    fix: dept entity

--- src/entities/depart.ts
+++ src/entities/depart.ts
@@ -1,4 +1,19 @@
-export interface DepartAttributes {
-  id: number;
-  dept: string;
+import { Model, DataType, Table, Column, Min, Max, NotEmpty } from "sequelize-typescript";
+
+@Table
+class Certificate extends Model {
+  @Column({
+    type: DataType.INTEGER,
+    primaryKey: true
+  })
+  @Min(1)
+  @Max(4)
+  id!: number;
+
+  @Column({
+    type: DataType.STRING(20),
+    allowNull: false
+  })
+  @NotEmpty
+  dept!: string;
 }
