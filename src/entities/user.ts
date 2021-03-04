f8dba7487afa1e5bae86de4b0590ccb3a72cc7f8
MinJunSeo <smj0385@naver.com>
2021-03-04 15:39:07 +0900

    fix: user entity

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,5 +1,22 @@
-export interface UserAttributes {
-  id: string;
-  password: string;
-  name: string;
+import { Model, DataType, Table, Column } from "sequelize-typescript";
+
+@Table
+class User extends Model {
+  @Column({
+    type: DataType.STRING(15),
+    primaryKey: true
+  })
+  id!: string;
+
+  @Column({
+    type: DataType.STRING(50),
+    allowNull: false
+  })
+  password!: string;
+
+  @Column({
+    type: DataType.STRING(5),
+    allowNull: false
+  })
+  name!: string;
 }
