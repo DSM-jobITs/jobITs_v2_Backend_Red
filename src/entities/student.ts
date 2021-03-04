18c5df715ef7d98352194d1b17257a7508efafc2
MinJunSeo <smj0385@naver.com>
2021-03-04 17:14:22 +0900

    feat: model association

--- src/entities/student.ts
+++ src/entities/student.ts
@@ -1,11 +1,39 @@
-import { Model, DataType, Table, Column } from "sequelize-typescript";
+import { Model, DataType, Table, Column, BelongsTo } from "sequelize-typescript";
+import { Depart } from "./depart";
+import { User } from "./user";
 
 @Table
-class Student extends Model {
+export class Student extends Model {
   @Column({
     type: DataType.CHAR(4),
     primaryKey: true,
     field: 'std_no'
   })
   stdNo!: string;
+
+  @Column({
+    type: DataType.INTEGER,
+    field: 'deaprt_id'
+  })
+  deptId!: number;
+
+  @Column({
+    type: DataType.STRING(15),
+    field: 'user_id'
+  })
+  userId!: string;
+  
+  @BelongsTo(() => Depart, {
+    foreignKey: 'depart_id',
+    onUpdate: "CASCADE",
+    onDelete: "CASCADE"
+  })
+  depart!: Depart;
+
+  @BelongsTo(() => User, {
+    foreignKey: 'user_id',
+    onUpdate: "CASCADE",
+    onDelete: "CASCADE"
+  })
+  user!: User;
 }
