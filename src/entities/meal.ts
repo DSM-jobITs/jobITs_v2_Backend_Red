d0f4fefabbbe75d2487495ea03b96b0186ea0c9b
MinJunSeo <smj0385@naver.com>
2021-03-04 15:25:17 +0900

    fix: meal entity

--- src/entities/meal.ts
+++ src/entities/meal.ts
@@ -1,7 +1,39 @@
-export interface MealAttributes {
-  recuritId: string;
-  breakfast: boolean;
-  lunch: boolean;
-  dinner: boolean;
-  salary: boolean;
+import { Model, DataType, Table, Column } from "sequelize-typescript";
+
+@Table
+class Meal extends Model {
+  @Column({
+    type: DataType.STRING(30),
+    primaryKey: true,
+    field: 'recurit_id'
+  })
+  recuritId!: number;
+
+  @Column({
+    type: DataType.TINYINT,
+    allowNull: false,
+    defaultValue: false
+  })
+  breakfast!: boolean;
+
+  @Column({
+    type: DataType.TINYINT,
+    allowNull: false,
+    defaultValue: false
+  })
+  lunch!: boolean;
+
+  @Column({
+    type: DataType.TINYINT,
+    allowNull: false,
+    defaultValue: false
+  })
+  dinner!: boolean;
+
+  @Column({
+    type: DataType.TINYINT,
+    allowNull: false,
+    defaultValue: false
+  })
+  salary!: boolean;
 }
