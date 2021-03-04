e1445bad2c3a15da6a20489e48033a62289d9785
MinJunSeo <smj0385@naver.com>
2021-03-04 15:36:28 +0900

    fix: specialty entity

--- src/entities/specialty.ts
+++ src/entities/specialty.ts
@@ -1,5 +1,16 @@
-export interface SpecialtyAttributes {
-  id: string;
-  specialty: string;
-  qualificationId: string;
+import { Model, DataType, Table, Column } from "sequelize-typescript";
+
+@Table
+class Specialty extends Model {
+  @Column({
+    type: DataType.STRING(30),
+    primaryKey: true
+  })
+  id!: string;
+
+  @Column({
+    type: DataType.STRING(20),
+    allowNull: false
+  })
+  specialty!: string;
 }
