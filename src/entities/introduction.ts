00e82083d06034eda6782d0bbb1ff6f5daab7952
MinJunSeo <smj0385@naver.com>
2021-03-04 15:09:23 +0900

    fix: introduction entity

--- src/entities/introduction.ts
+++ src/entities/introduction.ts
@@ -1,6 +1,24 @@
-export interface IntroductionAttributes {
-  id: string;
-  originalName: string;
-  fileUuid: string;
-  entNo: string;
+import { Model, DataType, Table, Column } from "sequelize-typescript";
+
+@Table
+class Introduction extends Model {
+  @Column({
+    type: DataType.STRING(30),
+    primaryKey: true
+  })
+  id!: number;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    field: 'original_name'
+  })
+  originalName!: string;
+
+  @Column({
+    type: DataType.STRING(50),
+    allowNull: false,
+    field: 'file_uuid'
+  })
+  fileUuid!: string;
 }
