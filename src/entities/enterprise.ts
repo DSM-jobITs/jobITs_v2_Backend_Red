1bc2b96cd614a3ef2a2085132e765504830d0fc9
MinJunSeo <smj0385@naver.com>
2021-03-04 15:04:39 +0900

    fix: enterprise entity

--- src/entities/enterprise.ts
+++ src/entities/enterprise.ts
@@ -1,11 +1,41 @@
-export interface EnterpriseAttributes {
-  entNo: string;
-  name: string | null;
-  phone: string | null;
-  establishmentDate: string | null;
-  sales: number | null;
-  introduce: string | null;
-  sector: string | null;
-  address: string | null;
-  zipCode: string | null;
+import { Model, DataType, Table, Column } from "sequelize-typescript";
+
+@Table
+class Enterprise extends Model {
+  @Column({
+    type: DataType.CHAR(12),
+    primaryKey: true,
+    field: 'ent_no'
+  })
+  entNo!: string;
+
+  @Column(DataType.STRING(30))
+  name!: string;
+
+  @Column(DataType.CHAR(13))
+  phone!: string;
+
+  @Column({
+    type: DataType.STRING(15),
+    field: 'establishment_date'
+  })
+  establishmentDate!: string;
+
+  @Column(DataType.FLOAT)
+  sales!: number;
+
+  @Column(DataType.STRING)
+  introduce!: string;
+
+  @Column(DataType.STRING(50))
+  sector!: string;
+
+  @Column(DataType.STRING(40))
+  address!: string;
+
+  @Column({
+    type: DataType.CHAR(5),
+    field: 'zip_code'
+  })
+  zipCode
 }
