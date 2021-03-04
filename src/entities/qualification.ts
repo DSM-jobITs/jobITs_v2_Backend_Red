5cb51e508c944aa06f648d2c5f3031fcf27fc04b
MinJunSeo <smj0385@naver.com>
2021-03-04 15:28:10 +0900

    fix: qualification entity

--- src/entities/qualification.ts
+++ src/entities/qualification.ts
@@ -1,5 +1,18 @@
-export interface QualificationAttributes {
-  id: string;
-  grade: number;
-  recuritId: string;
+import { Model, DataType, Table, Column, Min, Max } from "sequelize-typescript";
+
+@Table
+class Qualification extends Model {
+  @Column({
+    type: DataType.STRING(30),
+    primaryKey: true
+  })
+  id!: string;
+
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false
+  })
+  @Min(1)
+  @Max(100)
+  grade!: number;
 }
