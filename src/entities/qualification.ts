4802754b20d1b826bb00b3e31001e20f493c6007
MinJunSeo <smj0385@naver.com>
2021-03-04 16:57:38 +0900

    feat: model association

--- src/entities/qualification.ts
+++ src/entities/qualification.ts
@@ -1,4 +1,7 @@
-import { Model, DataType, Table, Column, Min, Max } from "sequelize-typescript";
+import { Model, DataType, Table, Column, Min, Max, ForeignKey, BelongsTo, HasMany } from "sequelize-typescript";
+import { Recurit } from "./recurit";
+import { Certificate } from "./certificate";
+import { Specialty } from "./specialty";
 
 @Table
 export class Qualification extends Model {
@@ -16,4 +19,31 @@ export class Qualification extends Model {
   @Min(1)
   @Max(100)
   grade!: number;
+
+  @ForeignKey(() => Recurit)
+  @Column({
+    type: DataType.STRING(30),
+    allowNull: false
+  })
+  recuritId!: string;
+
+  @HasMany(() => Certificate, {
+    foreignKey: 'qualification_id',
+    onUpdate: "CASCADE",
+    onDelete: "CASCADE"
+  })
+  certificates!: Certificate[];
+
+  @HasMany(() => Specialty, {
+    foreignKey: 'qualification_id',
+    onUpdate: "CASCADE",
+    onDelete: "CASCADE"
+  })
+  specialties!: Specialty[];
+
+  @BelongsTo(() => Recurit, {
+    onUpdate: "CASCADE",
+    onDelete: "CASCADE"
+  })
+  recurit!: Recurit;
 }
