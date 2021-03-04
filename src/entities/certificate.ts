0249de3b47826ace73b2799a983f07a380cacdbb
MinJunSeo <smj0385@naver.com>
2021-03-04 14:46:37 +0900

    fix: certificate entity

--- src/entities/certificate.ts
+++ src/entities/certificate.ts
@@ -1,5 +1,29 @@
-export interface CertificateAttributes {
-  id: string;
-  certificate: string;
-  qualificationId: string;
+import { QualificationRepository } from "repositories";
+import {
+  Model,
+  DataType,
+  Table,
+  Column,
+  BelongsTo
+} from "sequelize-typescript";
+
+
+@Table
+class Certificate extends Model {
+  @Column({
+    type: DataType.STRING(30),
+    primaryKey: true
+  })
+  id!: string;
+
+  @Column({
+    type: DataType.STRING(20)
+  })
+  certificate!: string;
+
+  @Column({
+    type: DataType.STRING(30),
+    field: 'qualification_id'
+  })
+  qualificationId!: string;
 }
