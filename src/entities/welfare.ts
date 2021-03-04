9e451970be9f390c268a2afb9fb3d0fc871d6306
MinJunSeo <smj0385@naver.com>
2021-03-04 15:41:25 +0900

    fix: welfare entity

--- src/entities/welfare.ts
+++ src/entities/welfare.ts
@@ -1,7 +1,37 @@
-export interface WelfareAttributes {
-  recuritId: string;
-  fourMajor: boolean;
-  selfDevelop: boolean;
-  laptop: boolean;
-  etc: string | null;
+import { Model, DataType, Table, Column } from "sequelize-typescript";
+
+@Table
+class Welfare extends Model {
+  @Column({
+    type: DataType.STRING(30),
+    primaryKey: true,
+    field: 'recurit_id'
+  })
+  recuritId!: string;
+
+  @Column({
+    type: DataType.TINYINT,
+    allowNull: false,
+    defaultValue: false,
+    field: 'four_major'
+  })
+  fourMajor!: boolean;
+
+  @Column({
+    type: DataType.TINYINT,
+    allowNull: false,
+    defaultValue: false,
+    field: 'self_develop'
+  })
+  selfDevelop!: boolean;
+
+  @Column({
+    type: DataType.TINYINT,
+    allowNull: false,
+    defaultValue: false
+  })
+  laptop!: boolean;
+
+  @Column(DataType.STRING)
+  etc!: string;
 }
