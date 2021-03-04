6b247d834172800ef5223072c903f32544bb9106
MinJunSeo <smj0385@naver.com>
2021-03-04 15:35:17 +0900

    fix: recurit entity

--- src/entities/recurit.ts
+++ src/entities/recurit.ts
@@ -1,16 +1,65 @@
-export interface RecuritAttributes {
-  id: string;
-  reception: string | null;
-  deadline: string | null;
-  recuritPlan: boolean | null;
-  startTime: string | null;
-  endTime: string | null;
-  salary: number | null;
-  period: number | null;
-  expired: boolean | null;
-  personnel: number | null;
-  detail: string | null;
-  writing: boolean | null;
-  page: number;
-  entNo: string;
+import { Model, DataType, Table, Column } from "sequelize-typescript";
+
+@Table
+class Recurit extends Model {
+  @Column({
+    type: DataType.STRING(30),
+    primaryKey: true
+  })
+  id!: string;
+
+  @Column(DataType.CHAR(10))
+  reception!: string;
+
+  @Column(DataType.CHAR(10))
+  deadline!: string;
+
+  @Column({
+    type: DataType.TINYINT,
+    field: 'recurit_plan'
+  })
+  recuritPlan!: boolean;
+
+  @Column({
+    type: DataType.CHAR(5),
+    field: 'start_time'
+  })
+  startTime!: string;
+
+  @Column({
+    type: DataType.CHAR(5),
+    field: 'end_time'
+  })
+  endTime!: string;
+
+  @Column(DataType.INTEGER)
+  salary!: number;
+
+  @Column(DataType.INTEGER)
+  period!: number;
+
+  @Column({
+    type: DataType.TINYINT,
+    defaultValue: false
+  })
+  expired!: boolean;
+
+  @Column(DataType.INTEGER)
+  personnel!: number;
+
+  @Column(DataType.STRING)
+  detail!: string;
+
+  @Column({
+    type: DataType.TINYINT,
+    defaultValue: false
+  })
+  writing!: boolean;
+
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+    defaultValue: 1
+  })
+  page!: number;
 }
