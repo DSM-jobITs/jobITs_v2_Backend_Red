db5b26816f0ce4e74a78d116dcce4aa8b4800449
MinJunSeo <smj0385@naver.com>
2021-03-05 19:25:29 +0900

    fix: schema name

--- src/schemas/recurit.ts
+++ src/schemas/recurit.ts
@@ -1,9 +1,9 @@
 import Joi from "joi";
 
-const recuritIdSchema = Joi.object().keys({
+const recruitIdSchema = Joi.object().keys({
   recuritId: Joi.string()
     .length(30)
-    .pattern(new RegExp('^[0-9]{1,30}$'))
+    .alphanum()
 });
 
-export { recuritIdSchema };
+export { recruitIdSchema };
