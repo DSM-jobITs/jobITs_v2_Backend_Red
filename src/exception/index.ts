26c367995c3a02ba709d7cffaade1901a7d7587d
MinJunSeo <smj0385@naver.com>
2021-03-05 11:53:25 +0900

    feat: recurit not found error entity

--- src/exception/index.ts
+++ src/exception/index.ts
@@ -2,3 +2,4 @@ import HttpError from "./httpError";
 
 export const BadRequest = new HttpError(400, "Bad Request");
 export const ApiNotFound = new HttpError(404, "Api Not Found");
+export const RecuritNotFound = new HttpError(404, "Recurit Not Found");
