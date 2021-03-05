241516ecd97562fc0f3e1d216a253a7d65dc3e9f
MinJunSeo <smj0385@naver.com>
2021-03-05 13:05:27 +0900

    feat: recurit response interface

---
+++ src/interfaces/response/recurit.ts
@@ -0,0 +1,59 @@
+export interface getRecuritResponse {
+  entName: string;
+  entNo: string;
+  deadline: string;
+  introduction: string;
+  workContent: string;
+  qualification: Qualification.QualificationObject;
+  workingConditions: WorkingConditions.WorkingConditionsObject;
+  entInfo: EntInfo.EntInfoObject;
+  other: Other.OtherObject;
+}
+
+namespace Qualification {
+  type QualificationKey = "certificate" | "grade" | "specialty";
+  export type QualificationObject = {
+    [key in QualificationKey]: Array<string> | string | null;
+  }
+}
+
+namespace WorkingConditions {
+  type WorkingConditionsKey = "salary" | "period" | "meal" | "welfare";
+  namespace Meal {
+    type MealKey = "breakfast" | "lunch" | "dinner" | "includeSalary";  
+    export type MealObject = {
+      [key in MealKey]: boolean;
+    }
+  }
+  namespace Welfare {
+    type WelfareKey = "fourMajor" | "selfDevelop" | "labtop" | "etc";
+    export type WelfareObject = {
+      [key in WelfareKey]: boolean | string;
+    }
+  }
+
+  export type WorkingConditionsObject = {
+    [key in WorkingConditionsKey]: number | Meal.MealObject | Welfare.WelfareObject;
+  }
+}
+
+namespace EntInfo {
+  type EntInfoKey = "numOfWorker" | "entPhone" | "entSales" | "address" | "establishmentDate" | "startTime" | "endTime";
+  export type EntInfoObject = {
+    [key in EntInfoKey]: number | string;
+  }
+}
+
+namespace Other {
+  type OtherKey = "personnel" | "recuritPlan" | "reception" | "file";
+  namespace File {
+    type FileKey = "name" | "url";
+    export type FileObject = {
+      [key in FileKey]: string;
+    }
+  }
+
+  export type OtherObject = {
+    [name in OtherKey]: number | boolean | string | File.FileObject;
+  }
+}
