05df5c284fd2fe5b99cb3674393f9e6314149889
MinJunSeo <smj0385@naver.com>
2021-03-05 11:41:28 +0900

    feat: join table name alias

--- src/repositories/recurit.ts
+++ src/repositories/recurit.ts
@@ -29,16 +29,19 @@ export class RecuritRepository {
       include: [
         {
           model: Welfare,
+          as: 'welfare',
           required: true,
           attributes: ['four_major', 'self_develop', 'labtop', 'etc']
         },
         {
           model: Meal,
+          as: 'meal',
           required: true,
           attributes: ['breakfast', 'lunch', 'dinner', 'salary']
         },
         {
           model: Qualification,
+          as: 'qualification',
           attributes: ['grade'],
           include: [
             {
@@ -53,10 +56,12 @@ export class RecuritRepository {
         },
         {
           model: Enterprise,
+          as: 'entInfo',
           required: true,
           attributes: ['name', 'phone', 'establishment_date', 'sales', 'introduce', 'address'],
           include: [{
             model: Introduction,
+            as: 'file',
             required: true,
             attributes: [['original_name', 'name'], ['file_uuid', 'url']]
           }]
