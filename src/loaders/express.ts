617f95fe1c029659d6c053e516d79d188db67232
MinJunSeo <smj0385@naver.com>
2021-03-05 18:56:32 +0900

    feat: routing

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -3,12 +3,15 @@ import morgan from "morgan";
 import cors from "cors";
 import { ApiNotFound } from "../exception";
 import logger from "./logger";
+import router from "../routes";
 
 export default (app: express.Application) => {
   app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(morgan("dev"));
+
+  app.use('/', router());
 
   app.use((req, res, next) => {
     next(ApiNotFound);
