270e0148683d3ca3b5803918a4e403c485ca3acf
MinJunSeo <smj0385@naver.com>
2021-03-05 16:28:37 +0900

    feat: recurit controller

---
+++ src/controllers/recurit.ts
@@ -0,0 +1,17 @@
+import { RecuritService } from "../services";
+import { RecuritRepository } from "../repositories";
+import { getRecuritResponse } from "../interfaces";
+import { Request, Response, NextFunction } from "express";
+
+export class RecuritController {
+  public async getRecuritInfoById(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    const recuritId = req.params.recuritId;
+    const result = await new RecuritService(new RecuritRepository(), {} as getRecuritResponse)
+      .getRecuritInfoById(recuritId);
+    res.status(200).json(result);
+  }
+}
