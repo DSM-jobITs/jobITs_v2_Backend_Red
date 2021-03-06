b3e8668c21517117be8dc4669920ed6c2061a49e
MinJunSeo <smj0385@naver.com>
2021-03-06 20:58:49 +0900

    feat: getRecruitInfoById

--- src/repositories/recruit.ts
+++ src/repositories/recruit.ts
@@ -13,55 +13,58 @@ export class RecruitRepository {
   public async getRecruitInfoById(recruitId: string) {
     return await Recruit.findOne({
       attributes: [
-        'ent_no',
-        'deadline',
-        ['detail', 'workContent'],
-        'salary',
-        'period',
-        'start_time',
-        'end_time',
-        'personnel',
-        'reception'
+        "ent_no",
+        "deadline",
+        ["detail", "workContent"],
+        "salary",
+        "period",
+        "start_time",
+        "end_time",
+        "personnel",
+        "reception"
       ],
       where: {
         recruitId
       },
       include: [
         {
-          model: Welfare,
+          model: Enterprise,
           required: true,
-          attributes: ['four_major', 'self_develop', 'labtop', 'etc']
-        },
-        {
-          model: Meal,
-          required: true,
-          attributes: ['breakfast', 'lunch', 'dinner', 'salary']
-        },
-        {
-          model: Qualification,
-          attributes: ['grade'],
+          attributes: ["name", "phone", "establishment_date", "sales", "introduce", "address"],
           include: [
             {
-              model: Specialty,
+              model: Introduction,
               required: false,
-              attributes: ['specialty']
-            },
-            {
-              model: Certificate,
-              required: false,
-              attributes: ['certificate']
+              attributes: [["original_name", "name"], ["file_uuid", "url"]]
             }
           ]
         },
         {
-          model: Enterprise,
+          model: Qualification,
+          required: false,
+          attributes: ["grade"],
+          include: [
+            {
+              model: Certificate,
+              required: false,
+              attributes: ["certificate"]
+            },
+            {
+              model: Specialty,
+              required: false,
+              attributes: ["specialty"]
+            }
+          ]
+        },
+        {
+          model: Meal,
           required: true,
-          attributes: ['name', 'phone', 'establishment_date', 'sales', 'introduce', 'address'],
-          include: [{
-            model: Introduction,
-            required: true,
-            attributes: [['original_name', 'name'], ['file_uuid', 'url']]
-          }]
+          attributes: ["breakfast", "lunch", "dinner", "salary"]
+        },
+        {
+          model: Welfare,
+          required: true,
+          attributes: ["four_major", "self_develop", "laptop", "etc"]
         }
       ]
     });
