37a50a236c7145732b43a4a3b45160669f1c568a
MinJunSeo <smj0385@naver.com>
2021-03-06 15:37:10 +0900

    style: string use "

--- src/entities/student.ts
+++ src/entities/student.ts
@@ -14,25 +14,25 @@ export class Student extends Model {
   @PrimaryKey
   @Column({
     type: DataType.CHAR(4),
-    field: 'std_no'
+    field: "std_no"
   })
   stdNo!: string;
 
   @Column({
     type: DataType.INTEGER,
-    field: 'deaprt_id'
+    field: "deaprt_id"
   })
   deptId!: number;
 
   @Column({
     type: DataType.STRING(15),
-    field: 'user_id'
+    field: "user_id"
   })
   userId!: string;
   
-  @BelongsTo(() => Depart, 'depart_id')
+  @BelongsTo(() => Depart, "deaprt_id")
   depart!: Depart;
 
-  @BelongsTo(() => User, 'user_id')
+  @BelongsTo(() => User, "user_id")
   user!: User;
 }
