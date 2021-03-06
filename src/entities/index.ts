9421afe8ff5654bcd17ca6cff4d9bfe57da09ab8
MinJunSeo <smj0385@naver.com>
2021-03-06 16:50:34 +0900

    fix: join column missing error

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -16,70 +16,110 @@ Depart.hasMany(Student, {
   onUpdate: "CASCADE",
   onDelete: "CASCADE"
 });
-Student.belongsTo(Depart);
+Student.belongsTo(Depart, {
+  foreignKey: "depart_id",
+  onUpdate: "CASCADE",
+  onDelete: "CASCADE"
+});
 
 User.hasOne(Student, {
   foreignKey: "user_id",
   onUpdate: "CASCADE",
   onDelete: "CASCADE"
 });
-Student.belongsTo(User);
+Student.belongsTo(User, {
+  foreignKey: "user_id",
+  onUpdate: "CASCADE",
+  onDelete: "CASCADE"
+});
 
 Enterprise.hasOne(Manager, {
   foreignKey: "ent_no",
   onUpdate: "CASCADE",
   onDelete: "CASCADE"
 });
-Manager.belongsTo(Enterprise);
+Manager.belongsTo(Enterprise, {
+  foreignKey: "ent_no",
+  onUpdate: "CASCADE",
+  onDelete: "CASCADE"
+});
 
 Enterprise.hasMany(Recruit, {
   foreignKey: "ent_no",
   onUpdate: "CASCADE",
   onDelete: "CASCADE"
 });
-Recruit.belongsTo(Enterprise);
+Recruit.belongsTo(Enterprise, {
+  foreignKey: "ent_no",
+  onUpdate: "CASCADE",
+  onDelete: "CASCADE"
+});
 
 Enterprise.hasMany(Introduction, {
   foreignKey: "ent_no",
   onUpdate: "CASCADE",
   onDelete: "CASCADE"
 });
-Introduction.belongsTo(Enterprise);
+Introduction.belongsTo(Enterprise, {
+  foreignKey: "ent_no",
+  onUpdate: "CASCADE",
+  onDelete: "CASCADE"
+});
 
 Recruit.hasOne(Welfare, {
   foreignKey: "recruit_id",
   onUpdate: "CASCADE",
   onDelete: "CASCADE"
 });
-Welfare.belongsTo(Recruit);
+Welfare.belongsTo(Recruit, {
+  foreignKey: "recruit_id",
+  onUpdate: "CASCADE",
+  onDelete: "CASCADE"
+});
 
 Recruit.hasOne(Meal, {
   foreignKey: "recruit_id",
   onUpdate: "CASCADE",
   onDelete: "CASCADE"
 });
-Meal.belongsTo(Recruit);
+Meal.belongsTo(Recruit, {
+  foreignKey: "recruit_id",
+  onUpdate: "CASCADE",
+  onDelete: "CASCADE"
+});
 
 Recruit.hasOne(Qualification, {
   foreignKey: "recruit_id",
   onUpdate: "CASCADE",
   onDelete: "CASCADE"
 });
-Qualification.belongsTo(Recruit);
+Qualification.belongsTo(Recruit, {
+  foreignKey: "recruit_id",
+  onUpdate: "CASCADE",
+  onDelete: "CASCADE"
+});
 
 Qualification.hasMany(Certificate, {
   foreignKey: "qualification_id",
   onUpdate: "CASCADE",
   onDelete: "CASCADE"
 });
-Certificate.belongsTo(Qualification);
+Certificate.belongsTo(Qualification, {
+  foreignKey: "qualification_id",
+  onUpdate: "CASCADE",
+  onDelete: "CASCADE"
+});
 
 Qualification.hasMany(Specialty, {
   foreignKey: "qualification_id",
   onUpdate: "CASCADE",
   onDelete: "CASCADE"
 });
-Specialty.belongsTo(Qualification);
+Specialty.belongsTo(Qualification, {
+  foreignKey: "qualification_id",
+  onUpdate: "CASCADE",
+  onDelete: "CASCADE"
+});
 
 export {
   Certificate,
