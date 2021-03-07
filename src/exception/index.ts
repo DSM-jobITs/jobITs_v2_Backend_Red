b37848cd575f479118546ad71a3f27b9c307f9f0
MinJunSeo <smj0385@naver.com>
2021-03-07 20:34:54 +0900

    revert: remove forbidden token exception

--- src/exception/index.ts
+++ src/exception/index.ts
@@ -2,6 +2,5 @@ import HttpError from "./httpError";
 
 export const BadRequest = new HttpError(400, "Bad Request");
 export const TokenRequired = new HttpError(401, "Token Required");
-export const ForbiddenToken = new HttpError(403, "Forbidden Token");
 export const ApiNotFound = new HttpError(404, "Api Not Found");
 export const RecruitNotFound = new HttpError(404, "Recruit Not Found");
