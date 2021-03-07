bd6676c025a180cc374da7c5193743556c9478a5
MinJunSeo <smj0385@naver.com>
2021-03-07 10:20:50 +0900

    fix: recruitId params check options

--- src/schemas/recruit.ts
+++ src/schemas/recruit.ts
@@ -1,7 +1,7 @@
 import Joi from "joi";
 
 const recruitIdSchema = Joi.object().keys({
-  recruitId: Joi.string().length(30).alphanum()
+  recruitId: Joi.string().alphanum().max(30)
 });
 
 export { recruitIdSchema };
