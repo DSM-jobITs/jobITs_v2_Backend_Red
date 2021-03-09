35bb85833dc748fe161aec2cd8460396f8b38b32
MinJunSeo <smj0385@naver.com>
2021-03-09 11:34:16 +0900

    feat: testing routing

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,9 @@ import recruit from "./recruit";
 
 export default () => {
   const app = Router();
-  
+  app.use('/ping', (req, res, next) => {
+    res.send('pong');
+  });
   recruit(app);
 
   return app;
